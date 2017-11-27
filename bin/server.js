9a69f4922188b807a22e4f9f67c38fd22db51595
Pavel Ivanov <pablospaniard@gmail.com>
2017-11-27 20:16:29 +0100

    issue

--- bin/server.js
+++ bin/server.js
@@ -3,8 +3,9 @@ const browserSync = require('browser-sync').create();
 browserSync.init({
   server: {
       port: process.env.PORT || 5000,
-      host: process.env. IP || "localhost",
       "baseDir": ["./", "./app"]
   },
-    open: false
+    open: "external"
 });
+
+
