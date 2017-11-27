b7d7a2eb4b75133bddf4a158b1b6178c2e94a67e
Pavel Ivanov <pablospaniard@gmail.com>
2017-11-27 18:19:25 +0100

    port issue

--- bin/server.js
+++ bin/server.js
@@ -2,6 +2,7 @@ const browserSync = require('browser-sync').create();
 
 browserSync.init({
   server: {
+      port: process.env.PORT || 5000,
     "baseDir": ["./app", "./" ]
   },
     open: false
